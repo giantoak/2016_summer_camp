2c7ccd6cf4ef2c9501ed2ab84c348f1162cb2beb
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-07-26 01:02:11 -0400

    Planning comment, HBase lib.

--- classifier/make_image_df.py
+++ classifier/make_image_df.py
@@ -1,6 +1,33 @@
+from sys import argv
+
 """
-Reads image data produced by Svebor
+* Get CDR image ids for images used in ads
+* Get similar CDR IDs in common set
+* Generate cluster DF
+* return
 """
+
+HBASE_ADDR = 'memex-hbase-master:8080'
+
+
+def _hbase_row_value(table, row_id, key_id):
+    """
+    :param str table: The name of the MEMEX HBase table
+    :param str row_id: The row to get from the table
+    :param str key_id: The key to get from the row
+    :returns: `str` -- The value in the desired key, or `None`
+    """
+    import requests
+    try:
+        hbase_url = 'http://{}/{}/{}/{}'.format(HBASE_ADDR, table, row_id, key_id)
+        r = requests.get(hbase_url)
+        if r.status_code == 200:
+            return r.text
+    except:
+        pass
+
+    return None
+
 
 def main():
     # df.to_pickle('data/generated/image_df.pkl')
