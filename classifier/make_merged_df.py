84299b071bf7841dcd8a39e80740231c582f55d3
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-07-26 01:29:03 -0400

    Merge covers all pickles, uses loops for clarity

--- classifier/make_merged_df.py
+++ classifier/make_merged_df.py
@@ -29,13 +29,26 @@ To do
 
 def main():
     import pandas as pd
+    from tqdm import tqdm
 
     df = pd.read_pickle('data/generated/lattice_df.pkl')
-    df_2 = pd.read_pickle('data/generated/homology_df.pkl')
-    df_3 = df.merge(df_2, left_on='_id', right_on='cdr_id', how='left')
-    del df, df_2
 
-    df_3.to_pickle('data/generated/merged_df.pkl')
+    pkls_to_merge = ['giantoak_df.pkl'
+                     'homology_df.pkl',
+                     'image_df.pkl',
+                     'author_df.pkl']
+
+    pbar = tqdm(pkls_to_merge)
+    for pkl_df in pbar:
+        pbar.set_description(pkl_df)
+        tmp_df = pd.read_pickle('data/generated/{}'.format(pkl_df))
+        df = pd.merge(df,
+                      left_on='_id',
+                      right_on='cdr_id',
+                      how='left')
+        del tmp_df
+
+    df.to_pickle('data/generated/merged_df.pkl')
 
 
 if __name__ == "__main__":
