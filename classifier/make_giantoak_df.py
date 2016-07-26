91148f757bf36e1abd968fbbf14d39315f426628
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-07-26 01:01:40 -0400

    Placeholder pickle comments

--- classifier/make_giantoak_df.py
+++ classifier/make_giantoak_df.py
@@ -3,6 +3,8 @@ Reads Giant Oak data produced by Jeff / Gabriel
 """
 
 def main():
+    # This should replace our use of the lattice_df.
+    # df.to_pickle('data/generated/giantoak_df.pkl')
     pass
 
 if __name__ == "__main__":
--- classifier/make_image_df.py
+++ classifier/make_image_df.py
@@ -3,6 +3,7 @@ Reads image data produced by Svebor
 """
 
 def main():
+    # df.to_pickle('data/generated/image_df.pkl')
     pass
 
 if __name__ == "__main__":
